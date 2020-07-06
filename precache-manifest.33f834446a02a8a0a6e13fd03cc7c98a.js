2fe444b6662d29d18e512459f303aeeae8625222
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-07-07 00:57:23 +0300

    Updates

--- precache-manifest.33f834446a02a8a0a6e13fd03cc7c98a.js
+++ precache-manifest.33f834446a02a8a0a6e13fd03cc7c98a.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f1d32f45004a673d2da4e93a32ec32c2",
+    "revision": "bbd3c94252f89ba6c619598571f45d1b",
     "url": "/todo/index.html"
   },
   {
-    "revision": "5e5f8a273474a62f50bf",
+    "revision": "b5e8ed392d7257be86b9",
     "url": "/todo/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "0898259e385c16bd4b22",
+    "revision": "b1952e7e98be58b454b9",
     "url": "/todo/static/css/main.b0dc41ba.chunk.css"
   },
   {
-    "revision": "5e5f8a273474a62f50bf",
-    "url": "/todo/static/js/2.8a99b19e.chunk.js"
+    "revision": "b5e8ed392d7257be86b9",
+    "url": "/todo/static/js/2.4e38f94c.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/todo/static/js/2.8a99b19e.chunk.js.LICENSE.txt"
+    "url": "/todo/static/js/2.4e38f94c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0898259e385c16bd4b22",
-    "url": "/todo/static/js/main.644de380.chunk.js"
+    "revision": "b1952e7e98be58b454b9",
+    "url": "/todo/static/js/main.8cf0a3cd.chunk.js"
   },
   {
     "revision": "4b7c96a078d3292937b1",
